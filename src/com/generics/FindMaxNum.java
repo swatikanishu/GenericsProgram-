f60b168ea699291d2d33fafc876f3ab16c841dce
swatikanishu <nishuswatika@gmail.com>
2022-06-29 21:52:19 +0530

    Added:modified code to print method

--- src/com/generics/FindMaxNum.java
+++ src/com/generics/FindMaxNum.java
@@ -3,45 +3,36 @@ package com.generics;
 
 import java.util.Arrays;
 
-public class FindMaxNum <T extends Comparable<T>> {
+public class FindMaxNum<T extends Comparable<T>> {
     private T[] inputArray;
-    FindMaxNum(T[] inputArray) { //generics method to accept Type from generics class array
-        this.inputArray = inputArray;//converting into this method object
+
+    FindMaxNum(T[] inputArray) {
+        this.inputArray = inputArray;
     }
-    public void maxElement() {
+
+    public void testMaximum() {
         for (int i = 0; i < inputArray.length - 1; i++) {
             if (inputArray[i].compareTo(inputArray[i + 1]) > 0) {
                 inputArray[i + 1] = inputArray[i];
             }
         }
+
+    }
+
+    private void toPrint() {
         System.out.println(inputArray[inputArray.length - 1]);
     }
-    public void display() {
-        for (int i = 0; i < inputArray.length; i++) {
-            System.out.println(inputArray[i]);
-        }
-        Arrays.sort(inputArray);
-        System.out.println("After Sorting");
-        for(int i =0; i< inputArray.length ; i++){
-            System.out.println(inputArray[i]);
-        }
-    }
+
     public static void main(String[] args) {
-
-        Integer[] i = {276, 310, 701, 923, 430, 32};
-        String[] s = {"Peach", "Banana", "Apple", "Grapes"};
-        Float[] f = {0.93f, 0.30f, 8.670f, 00.10f};
-        System.out.println("*******integer******");
-        new  FindMaxNum(i).display();
-        System.out.println("*******String******");
-        new  FindMaxNum(s).display();
-        System.out.println("*******Float******");
-        new  FindMaxNum(f).display();
-        System.out.println("Maximum From Three Value");
-        new  FindMaxNum(i).maxElement();
-        new  FindMaxNum(s).maxElement();
-        new  FindMaxNum(f).maxElement();
-
+        Integer[] i = {38, 40, 90};
+        String[] s = {"Peach", "Banana", "Apple"};
+        Float[] f = {1.56f, 3.30f, 6.37f};
+        new FindMaxNum(i).testMaximum();
+        new FindMaxNum(s).testMaximum();
+        new FindMaxNum(f).testMaximum();
+        new FindMaxNum(i).toPrint();
+        new FindMaxNum(s).toPrint();
+        new FindMaxNum(f).toPrint();
 
     }
 }
