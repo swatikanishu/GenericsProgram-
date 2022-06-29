a648b28e9a7eba728c88d2daef2008117aa62484
swatikanishu <nishuswatika@gmail.com>
2022-06-29 20:57:09 +0530

    Added: refactored code added

--- src/com/generics/FindMaxNum.java
+++ src/com/generics/FindMaxNum.java
@@ -1,8 +1,19 @@
 package com.generics;
 
-public class FindMaxNum {
 
-        public static  <T extends Comparable> T maxNumber(T num1, T num2, T num3) {
+    public class FindMaxNum<T extends Comparable<T>>{
+        public T num1, num2, num3;
+
+        public FindMaxNum(T num1, T num2, T num3) {
+            this.num1 = num1;
+            this.num2 = num2;
+            this.num3 = num3;
+        }
+
+        public FindMaxNum() {
+        }
+
+        public static <T extends Comparable<T>> T maxNumber(T num1, T num2, T num3) {
             if (num1.compareTo(num2) > 0 && num1.compareTo(num3) > 0) {
                 return num1;
             } else if (num2.compareTo(num1) > 0 && num2.compareTo(num3) > 0) {
@@ -10,12 +21,19 @@ public class FindMaxNum {
             } else
                 return num3;
         }
-
-        public static void main(String[] args) {
-            System.out.println("Maximum number out of three integers is: " + maxNumber(31, 34, 400));
-            System.out.println("Maximum number out of three float is: " + maxNumber(7.3f, 0.95f, 78f));
-            System.out.println("Maximum among three string is: " + maxNumber("Apple", "Peach", "Banana"));
+        public <T extends Comparable <T>> T testMaximum() {
+            return (T) FindMaxNum.maxNumber(num1, num2, num3);
         }
 
+
+
+    public static void main(String[] args) {
+        FindMaxNum compareInteger = new FindMaxNum(7278, 412, 17);
+        System.out.println("Maximum number out of three integers is: " + compareInteger.testMaximum());
+        FindMaxNum comapreFloat = new FindMaxNum (671.f, 0.289f, 1.456f);
+        System.out.println("Maximum number out of three float is: " + comapreFloat.testMaximum());
+        FindMaxNum compareString = new FindMaxNum("Apple", "Grapes", "Banana");
+        System.out.println("Maximum among three string is: " + compareString.testMaximum());
     }
+}
 
