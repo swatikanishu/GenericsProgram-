c121d0269ad6ca6bd740c987e5e0814894ee444a
swatikanishu <nishuswatika@gmail.com>
2022-06-29 09:38:54 +0530

    Added: code to find max ineger added

--- src/com/generics/FindMaxNum.java
+++ src/com/generics/FindMaxNum.java
@@ -1,7 +1,45 @@
 package com.generics;
+import java.util.Scanner;
 
 public class FindMaxNum {
     public static void main(String[] args){
-        System.out.println("Welcome to the Generics program");
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter three Integer");
+    int x = sc.nextInt();
+    int y = sc.nextInt();
+    int z = sc.nextInt();
+
+
+    Integer i1 = x;
+    Integer i2 = y;
+    Integer i3 = z;
+
+    int result1 = i1.compareTo(i2);
+    int result2=0;
+    int result3=0;
+        if (result1 < 0) {
+        result2 = i2.compareTo(i3);
+    } else if (result1 > 0) {
+        result3 = i1.compareTo(i3);
+    } else {
+        result2 = i1.compareTo(i3);
     }
+        if (result2 > 0) {
+        System.out.println(y);
+    } else if (result2 < 0) {
+        System.out.println("Max for all three value are"+" "+z);
+    } else if (result3>0){
+        System.out.println("Max for all three value are"+" "+x);
+    }
+        else if(result3<0)
+    {
+        System.out.println("Max for all three value are"+" "+z);
+    }
+        else
+    {
+        System.out.println("Max for all three value are"+" "+x);
+    }
+
 }
+}
+
